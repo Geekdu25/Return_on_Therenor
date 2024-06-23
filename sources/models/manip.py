42372a30377028057c3f4d6ace2ebc1643afb96f
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2024-06-23 07:20:51 +0200

    Add files via upload

--- sources/models/manip.py
+++ sources/models/manip.py
@@ -1,13 +1,24 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import *
+def write():
+	model.writeBamFile("maison_taya.bam")
+loadPrcFileData("", "want-directtools #t")
+loadPrcFileData("", "want-tk #t")
 base = ShowBase()
-model = loader.loadModel("Village.bam")
-#mon_modèle.setHpr(0, 270, 180)
-a = model.find("**/stems_3_713_1")
+model = loader.loadModel("maison_taya.bam")
+#------------------Changer l'orientation d'un modèle----------------------
+"mon_modèle.setHpr(0, 270, 180)"
+#--------------------Trouver un objet du modèle et le supprimer-------------
+"""a = model.find("**/chaussette_128_226_1")
 a.node().removeAllChildren()
 a.removeNode()
+a = model.find("**/Plane_321_183_1")
+a.node().removeAllChildren()
+a.removeNode()"""
+#----------------Changer la taille d'un modèle------------------
+"model.setScale(22)"
 model.reparentTo(render)
-render.setLight(render.attachNewNode(AmbientLight("alight")))
-model.writeBamFile("Village.bam")
+#render.setLight(render.attachNewNode(AmbientLight("alight")))
+base.accept("a", write)
 base.run()
 
