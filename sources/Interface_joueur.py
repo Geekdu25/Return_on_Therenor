2c7d2bd79803b0b78dc1b482d974d7d180395e90
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2024-12-12 14:56:47 +0100

    Ajout de l'inventaire de ElRyumi

---
+++ sources/Interface_joueur.py
@@ -0,0 +1,119 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+from direct.task.Task import Task
+from direct.actor.Actor import Actor
+from direct.gui.OnscreenText import OnscreenText
+from direct.showbase.DirectObject import DirectObject
+from direct.filter.CommonFilters import *
+from direct.gui.OnscreenImage import *
+import sys
+import os
+
+
+
+class InterfaceJoueur(ShowBase):
+    """Classe gérant l'interface des stats du joueur: barre de HP, de mana, et bourse"""
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        self.accept("mouse1", self.enlever_hp)  # Bouton gauche souris
+        self.accept("mouse3", self.ajouter_hp) # Bouton droit souris
+        self.accept("a", self.ajouter_mana)    #touche a
+        self.accept("z", self.enlever_mana)    #touche z
+        self.accept("r", self.enlever_argent)  #touche r
+        self.accept("e", self.ajouter_argent)  #touche e
+
+        #stats du joueur(on pourra les modifier si elles sont sensées être stockées ailleurs)
+        self.pv = 50
+        self.pv_max = 50
+        self.mana = 25
+        self.mana_max = 25
+        self.bourse = 100
+
+        self.hp = affichageTexte(0.6, 0.06, 0.05, 'HP')  #affichage à l'écran de "HP"
+        self.mn = affichageTexte(0.6, 0.131, 0.05, 'MANA')  #affichage à l'écran de "MANA"
+        self.no = OnscreenImage(image="noai.png", pos=(-1.2,0,0.73), scale=(0.05,0,0.05))
+        self.no.setTransparency(TransparencyAttrib.MAlpha)
+        self.ag = affichageTexte(0.2, 0.25, 0.1, str(self.bourse))  #affichage à l'écran de la quantité d'argent
+
+        self.barre_pv_root = NodePath("BarrePVRoot")
+        self.barre_pv_root.reparentTo(aspect2d)
+        self.barre_mana_root = NodePath("BarreManaRoot")
+        self.barre_mana_root.reparentTo(aspect2d)
+
+        self.create("background_pv",scale=(0.5,0,0.05),color=LVector4(0.2,0.2,0.2,1),pos=(-1.25,0,0.92))
+        self.barre_pv = self.create("Barredevie",scale=(0.5,0,0.05),color=LVector4(0.8,0,0,1),pos=(-1.25,0,0.92))
+        self.create("background_mana",scale=(0.5,0,0.05), color=LVector4(0.2,0.2,0.2,1),pos=(-1.25,0,0.85))
+        self.barre_mana = self.create("Barredemana",scale=(0.5,0,0.05), color=LVector4(0,0.8,0,1),pos=(-1.25,0,0.85))
+
+
+
+    def create(self, name, scale, color, pos):
+        """
+        Créer une barre rectangulaire"""
+        cm = CardMaker(name)
+        cm.setFrame(0,1,-0.5,0.5)
+        barre = NodePath(cm.generate())
+        barre.setScale(scale)
+        barre.setPos(pos)
+        barre.setColor(color)
+        barre.reparentTo(self.barre_pv_root)
+        return barre
+
+
+
+
+    def enlever_hp(self):
+        """Méthode qui diminue la quantité de la barre d'hp quand le joueur en perd"""
+        if self.pv -1 >= 0:
+            self.pv -= 1
+            ratio = self.pv/self.pv_max
+            self.barre_pv.setScale(ratio*0.5,0,0.05)
+
+    def ajouter_hp(self):
+        """Méthode qui augmente la quantité de la barre d'hp quand le joueur en gagne"""
+        if self.pv + 1 <= self.pv_max:
+            self.pv += 1
+            ratio = self.pv/self.pv_max
+            self.barre_pv.setScale(ratio*0.5,0,0.05)
+
+    def ajouter_mana(self):
+        """Méthode qui augmente la quantité de la barre de mana quand le joueur en gagne"""
+        if self.mana+1 <= self.mana_max:
+            self.mana += 1
+            ratio = self.mana/self.mana_max
+            self.barre_mana.setScale(ratio*0.5,0,0.05)
+
+    def enlever_mana(self):
+        """Méthode qui diminue la quantité de la barre de mana quand le joueur en perd"""
+        if self.mana -1 >= 0:
+            self.mana -= 1
+            ratio = self.mana/self.mana_max
+            self.barre_mana.setScale(ratio*0.5,0,0.05)
+
+    def enlever_argent(self):
+        """Méthode qui diminue la quantité d'argent affichée quand le joueur en perd"""
+        if self.bourse -1 >= 0:
+            self. bourse -= 1
+            self.ag.destroy()
+            self.ag = affichageTexte(0.2, 0.25, 0.1, str(self.bourse))
+
+    def ajouter_argent(self):
+        """Méthode qui augmente la quantité d'argent affichée quand le joueur de gagne"""
+        self.bourse += 1
+        self.ag.destroy()
+        self. ag = affichageTexte(0.2, 0.25, 0.1, str(self.bourse))
+
+
+
+def affichageTexte(x, y, t, texte):
+    """Fonction pour mettre du texte à l'écran
+    x, y coordonnées du texte, et t la taille"""
+    return OnscreenText(text=texte, style=1, fg=(1, 1, 1, 1), scale=t,
+                        shadow=(0, 0, 0, 1), parent=base.a2dTopLeft,
+                        pos=(x, -y - 0.04), align=TextNode.ALeft)
+
+base = InterfaceJoueur()
+base.run()
+
+
---
+++ sources/Inventaire.py
@@ -0,0 +1,261 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+from direct.task.Task import Task
+from direct.actor.Actor import Actor
+from direct.gui.OnscreenText import OnscreenText
+from direct.showbase.DirectObject import DirectObject
+from direct.filter.CommonFilters import *
+from direct.gui.OnscreenImage import *
+import sys
+import os
+
+class Inventaire(ShowBase):
+    """Classe gérant d'affichage à l'écran des deux inventaires: celui des armes et des items"""
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        self.weapons = ["épée","arc","sceptre"]  #liste des armes que l'on possède
+        self.arme_en_main = 0     #quand on a aucune arme équipée
+        self.weapon_texts = []    #liste servant à afficher les armes
+        self.visible = False  #état de l'inventaire (affiché ou non)
+        self.invent = None #rectangle de l'inventaire armes
+        self.titre = None #nom "ARMES"
+
+        self.inventaire = {'vodka':3, 'potion rouge':2, 'legume':1}   #liste des items de son inventaire avec leur quantité
+        self.item_selectione = 0    #item selectionné dans l'inventaire
+        self.inventaire_texts= []   #liste des textes à afficher
+        self.inventaire_liste = []    #liste des objets, pour la methode consommer_item
+        self.inventaire_visible = False   #état de l'inventaire (visible ou non)
+        self.invent2 = None   #rectangle de l'inventaire items
+        self.titre2 = None #nom "INVENTAIRE"
+
+        #commandes claviers utilisées:
+        self.accept("1", self.repartition_touches, [0])
+        self.accept("2", self.repartition_touches, [1])
+        self.accept("3", self.repartition_touches, [2])
+        self.accept("4", self.repartition_touches, [3])
+        self.accept("5", self.repartition_touches, [4])
+        self.accept("6", self.repartition_touches, [5])
+        self.accept("7", self.repartition_touches, [6])
+        self.accept("8", self.repartition_touches, [7])
+        self.accept("9", self.repartition_touches, [8])
+        self.accept("e", self.affiche_inventaire_armes)
+        self.accept("i", self.affiche_inventaire_items)
+        self.accept("c", self.consommer_item)
+
+        self.creer_inventaire()
+
+
+    def creer_inventaire(self):
+        """Créer l'affichage des deux inventaires"""
+
+        #Détruire le potentiel affichage d'un précédent inventaire
+        self.weapon_texts = []
+        self.invent = None
+        self.titre = None
+        self.inventaire_texts= []
+        self.inventaire_liste = []
+        self.invent2 = None
+        self.titre2 = None
+
+        #créer les rectangle
+        self.invent = self.creer_rectangle_inventaire(-0.71, -0.31, len(self.weapons))  #rectangle de l'inventaire des armes (limites en bas et en haut par défaut et la taille de l'inventaire
+        self.invent2 = self.creer_rectangle_inventaire(0.5, 0.8, len(self.inventaire))  #rectangle de l'inventaire des items (' ' ' ' ')
+
+        #créer les noms d'inventaires
+        self.titre = OnscreenText(text="ARMES",
+                                  pos=(-1.2, -0.6+len(self.weapons)*0.1 + 0.1),
+                                  scale=0.07,
+                                  fg=(1, 1, 1, 1),
+                                  align=TextNode.ALeft,
+                                  parent=aspect2d,
+                                  mayChange=True)
+        self.titre.hide()#on cache initialement le texte
+
+        self.titre2 = OnscreenText(text="INVENTAIRE",
+                                    pos=(-1.2, 0.7),
+                                    scale=0.07,
+                                    fg=(1,1,1,1),
+                                    align=TextNode.ALeft,
+                                    parent=aspect2d,
+                                    mayChange=True)
+        self.titre2.hide()#on cache initialement le texte
+
+        #créer l'inventaire(liste des armes puis des items à afficher)
+        for i, arme, in enumerate(self.weapons):
+            texte_arme = OnscreenText( text=f"{arme}",
+                                       pos=(-1.2, (-0.6 + len(self.weapons)*0.1)-0.1*(i+1)),
+                                       scale=0.05,
+                                       fg=(1,1,1,1),
+                                       align=TextNode.ALeft,
+                                       parent=aspect2d,
+                                       mayChange=True)
+            texte_arme.hide()#on cache initialement le texte
+            self.weapon_texts.append(texte_arme)
+
+        self.arme_surbrillance()#pour mettre l'arme selectionnée en surbrillance dés l'ouverture de l'inventaire
+
+        for i, item in enumerate(self.inventaire.keys()):
+            texte_item = OnscreenText( text=f"{item} x{self.inventaire[item]}",
+                                        pos=(-1.2, 0.55 - i*0.1),
+                                        scale=0.05,
+                                        fg=(1,1,1,1),
+                                        align=TextNode.ALeft,
+                                        parent=aspect2d,
+                                        mayChange=True)
+
+            texte_item.hide()#on cache initialement le texte
+            self.inventaire_texts.append(texte_item)
+            self.inventaire_liste.append(item)
+
+        self.item_surbrillance()#pour mettre l'objet selectionné en surbrillance dés l'ouverture de l'inventaire
+
+    def creer_rectangle_inventaire(self, bas, haut, taille):
+        """Méthode servant à créer les rectangles bleus servant de fond aux deux inventaires.
+        bas: position de la limite en bas du rectangle, qui variera pour le rectangle INVENTAIRE
+        haut: position de la limite en haut du rectangle, qui variera pour le rectangle ARMES
+        taille: taille de l'inventaire (nb d'éléments) servant à faire varier sa taille
+        """
+        if bas == -0.71:
+            haut = haut + 0.1*(taille-1)
+        if haut == 0.8:
+            bas = bas - 0.1*(taille-1)
+
+        cm = CardMaker("inventaire") #création du premier rectangle (bleu marine)
+        cm.setFrame(-1.24, -0.64, bas, haut)
+        inventaire = aspect2d.attachNewNode(cm.generate())
+        inventaire.setColor(0.1, 0.2, 0.5, 1)
+
+        cm_border = CardMaker("inventaire_bordure") #création du deuxième rectangle (bleu clair) servant de bordure, donc un peu plus large
+        cm_border.setFrame(-1.26, -0.62, bas-0.02, haut+0.02)
+        inventaire_bordure = aspect2d.attachNewNode(cm_border.generate())
+        inventaire_bordure.setColor(0.2, 0.4, 0.8, 1)
+        inventaire_bordure.setZ(-0.001)
+
+        #on rattache les deux rectangles ensembles
+        inventaire.reparentTo(inventaire_bordure)
+        inventaire_bordure.hide()#on cache initialement le rectangle
+        return inventaire_bordure
+
+    def arme_surbrillance(self):
+        """Sert à mettre l'arme selectionnée en surbrillance dans l'inventaire ARMES"""
+        for i, texte_arme in enumerate(self.weapon_texts):
+            if i==self.arme_en_main:
+                texte_arme.setFg((0,1,0,1))
+            else:
+                texte_arme.setFg((1,1,1,1))
+
+    def item_surbrillance(self):
+        """Sert à mettre l'item selectionné en surbrillance dans l'inventaire INVENTAIRE"""
+        for i, texte_item in enumerate(self.inventaire_texts):
+            if i==self.item_selectione:
+                texte_item.setFg((0,1,0,1))
+            else:
+                texte_item.setFg((1,1,1,1))
+
+
+    def repartition_touches(self, touche):
+        """Répartit la touche de clavier selectionnée, qui doit être utilisable pour les deux iventaires"""
+        if self.visible == True:
+            self.arme_select(touche)
+        if self.inventaire_visible == True:
+            self.item_select(touche)
+
+
+    def arme_select(self, indiceArme):
+        """Méthode qui modifie l'arme séléctionée et la met en surbrillance"""
+        if 0 <= indiceArme < len(self.weapons) and self.visible:
+            self.arme_en_main = indiceArme
+            self.arme_surbrillance()
+            print(f"Arme sélectionnée : {self.weapons[indiceArme]}")
+
+    def item_select(self, indiceItem):
+        """Méthode qui modifie l'item séléctioné et le met en surbrillance"""
+        if 0 <= indiceItem < len(self.inventaire) and self.inventaire_visible:
+            self.item_selectione = indiceItem
+            self.item_surbrillance()
+
+
+    def affiche_inventaire_armes(self):
+        """Méthode qui gère le changement d'état de l'inventaire des armes:
+            l'affiche avec afficher_armes() s'il ne l'est pas
+            le masque avec cacher_armes() s'il est affiché
+        """
+        if self.visible :
+            print("Iventaire fermé")
+            self.cacher_armes()
+            self.visible = False
+        else:
+            if self.inventaire_visible:
+                self.affiche_inventaire_items()
+            print("Inventaire ouvert")
+            self.afficher_armes()
+            self.visible = True
+
+    def afficher_armes(self):
+        """Méthode qui affiche les éléments consituant l'inventaire ARMES"""
+        self.titre.show()
+        self.invent.show()
+        for arme in self.weapon_texts:
+            arme.show()
+
+    def cacher_armes(self):
+        """Méthode qui masque les éléments consituant l'inventaire ARMES"""
+        self.invent.hide()
+        self.titre.hide()
+        for arme in self.weapon_texts:
+            arme.hide()
+
+    def affiche_inventaire_items(self):
+        """Méthode qui gère le changement d'état de l'inventaire des items:
+            l'affiche avec afficher_items() s'il ne l'est pas
+            le masque avec cacher_items() s'il est affiché
+            """
+        if self.inventaire_visible:
+            print("Inventaire fermé")
+            self.cacher_items()
+            self.inventaire_visible = False
+        else:
+            if self.visible:
+                self.affiche_inventaire_armes()
+            print("Inventaire ouvert")
+            self.afficher_items()
+            self.inventaire_visible = True
+
+    def afficher_items(self):
+        """Méthode qui affiche les éléments constituant l'inventaire INVENTAIRE"""
+        self.titre2.show()
+        self.invent2.show()
+        for item in self.inventaire_texts:
+            item.show()
+
+    def cacher_items(self):
+        """Méthode qui masque les éléments constituant l'inventaire INVENTAIRE"""
+        self.titre2.hide()
+        self.invent2.hide()
+        for item in self.inventaire_texts:
+            item.hide()
+
+    def consommer_item(self):
+        """Méthode qui permet de consommer un item pour décrémenter sa quantité et l'enlever de l'inventaire si sa quantité passe à 0"""
+        if self.inventaire_visible:
+            aConsommer = self.inventaire_liste[self.item_selectione]
+            self.inventaire[aConsommer] -= 1
+            if self.inventaire[aConsommer] == 0:
+                self.inventaire.pop(aConsommer)
+            self.cacher_items()
+            if self.item_selectione >= len(self.inventaire):
+                self.item_selectione -= 1
+            self.creer_inventaire()
+            self.afficher_items()
+            print(aConsommer+' consommé(e) !')
+
+
+
+game = Inventaire()
+game.run()
+
+
+
+
+
