0449fde8e0c13a6f3fa952fbda5aeed8cd6bcf80
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2024-12-18 17:41:05 +0100

    Changement du solide de collision du palmier

--- sources/objects.py
+++ sources/objects.py
@@ -119,9 +119,9 @@ class Palmier:
         return -> Palmier
         """
         self.object = loader.loadModel("Palmier.bam")
-        self.object.setScale(10)
+        self.object.setScale(13)
         self.col = CollisionNode("palmier")
-        self.col.addSolid(CollisionBox((0, 0, 3), 2.5, 2.5, 4))
+        self.col.addSolid(CollisionBox((0, 0, 3), 3, 3, 10))
         self.col.setFromCollideMask(BitMask32.allOff())
         self.col.setIntoCollideMask(BitMask32.bit(0))
         self.col_np = self.object.attachNewNode(self.col)
