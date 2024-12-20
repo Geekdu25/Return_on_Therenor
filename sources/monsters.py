ca0f200b7e351fc8f487426ea6e52e747df2c810
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2024-12-20 16:43:54 +0100

    Add files via upload

--- sources/monsters.py
+++ sources/monsters.py
@@ -10,7 +10,7 @@ class Monster(Actor):
     """
     Classe d'un monstre on ne peut plus basique.
     """
-    def __init__(self, name="slime", vies=3, actions=[]):
+    def __init__(self, name="slime", vies=3, actions=["move"]):
         """
         Méthode constructeur.
         ------------------------
@@ -23,36 +23,38 @@ class Monster(Actor):
         for anim in actions:
             if os.path.exists(f"../data/models/{name}-{anim}.bam"):
                 dico[anim] = f"../data/models/{name}-{anim}.bam"
-        Actor.__init__(self, name, dico)    
-        self.name = name        
+        Actor.__init__(self, name+".bam", dico)
+        self.name = name
         self.setScale(6)
         self.col = CollisionNode(name)
-        self.col.addSolid(CollisionSphere((0, 0, 1.5), 1.25)) 
-        self.col.setIntoCollideMask(BitMask32.bit(0)) 
+        self.col.addSolid(CollisionSphere((0, 1.5, 4.5), 2.5))
+        self.col.setIntoCollideMask(BitMask32.bit(0))
         self.col_np = self.attachNewNode(self.col)
+        self.col_np.show()
         self.vies = vies
 
-#------------------------------Tous les monstres-------------------------------     
+#------------------------------Tous les monstres-------------------------------
 class Slime(Monster):
     def __init__(self):
-        Monster.__init__(self)  
-        
+        Monster.__init__(self)
+        self.setHpr(0, 90, 0)
+
 class Ours(Monster):
     def __init__(self):
-        Monster.__init__(self, name="ours", vies=6) 
-        
+        Monster.__init__(self, name="ours", vies=6)
+
 class Arachnide(Monster):
     def __init__(self):
         Monster.__init__(self, name="arachnide", vies=4)
-        
+
 class Gobelin(Monster):
     def __init__(self):
-        Monster.__init__(self, name="gobelin", vies=4)                                      
-        
+        Monster.__init__(self, name="gobelin", vies=4)
+
 class Esprit(Monster):
     def __init__(self):
         Monster.__init__(self, name="esprit", vies=5)
-        
+
 class Requin(Monster):
     def __init__(self):
         Monster.__init__(self, name="requin", vies=6)
@@ -60,43 +62,43 @@ class Requin(Monster):
 class Homme_poisson(Monster):
     def __init__(self):
         Monster.__init__(self, name="homme_poisson", vies=5)
-        
+
 class Crabe_geant(Monster):
     def __init__(self):
         Monster.__init__(self, name="crabe_geant", vies=4)
-        
+
 class Serpent_des_mers(Monster):
     def __init__(self):
         Monster.__init__(self, name="serpents_des_mers", vies=8)
-        
+
 class Golem_marin(Monster):
     def __init__(self):
-        Monster.__init__(self, name="golem_marin", vies=8)  
-        
+        Monster.__init__(self, name="golem_marin", vies=8)
+
 class Yeti(Monster):
     def __init__(self):
-        Monster.__init__(self, name="yeti", vies=10)                                                                        
+        Monster.__init__(self, name="yeti", vies=10)
 
 class Loup_des_neiges(Monster):
     def __init__(self):
         Monster.__init__(self, name="loup_des_neiges", vies=6)
-        
+
 class Zombie_de_glace(Monster):
     def __init__(self):
         Monster.__init__(self, name="zombie_de_glace", vies=6)
-        
+
 class Sorcier_des_montagnes(Monster):
     def __init__(self):
         Monster.__init__(self, name="sorcier_des_montagnes", vies=12)
-        
+
 class Golem_de_glace(Monster):
     def __init__(self):
         Monster.__init__(self, name="golem_de_glace", vies=12)
-        
+
 class Gibdo(Monster):
     def __init__(self):
         Monster.__init__(self, name="gibdo", vies=10)
-        
+
 class Squelette(Monster):
     def __init__(self):
         Monster.__init__(self, name="squelette", vies=8)
@@ -104,39 +106,39 @@ class Squelette(Monster):
 class Scorpion_geant(Monster):
     def __init__(self):
         Monster.__init__(self, name="scorpion_geant", vies=10)
-        
+
 class Cobra(Monster):
     def __init__(self):
         Monster.__init__(self, name="cobra", vies=5)
-        
+
 class Ver_du_sable(Monster):
     def __init__(self):
         Monster.__init__(self, name="ver_du_sable", vies=5)
-        
+
 class Dragon(Monster):
     def __init__(self):
         Monster.__init__(self, name="dragon", vies=15)
-        
+
 class Spectre_brulant(Monster):
     def __init__(self):
         Monster.__init__(self, name="spectre_brulant", vies=10)
-        
+
 class Diablotin(Monster):
     def __init__(self):
         Monster.__init__(self, name="diablotin", vies=12)
 
 class Phoenix(Monster):
     def __init__(self):
-        Monster.__init__(self, name="phoenix", vies=13) 
-        
+        Monster.__init__(self, name="phoenix", vies=13)
+
 class Homme_des_cendres(Monster):
     def __init__(self):
         Monster.__init__(self, name="homme_des_cendres", vies=10)
-        
+
 class Zmeyevick(Monster):
     def __init__(self):
         Monster.__init__(self, name="zmeyevick", vies=20)
-        
+
 class Sorcier(Monster):
     def __init__(self):
-        Monster.__init__(self, name="sorcier", vies=15)                                                                                                                                                                         
+        Monster.__init__(self, name="sorcier", vies=15)
