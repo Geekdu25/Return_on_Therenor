bbd46209975c646d7ab2dbb03b387474d734ae25
elRyumi <remymartinot9@gmail.com>
2025-01-25 17:29:12 +0100

    Update Application.py

--- sources/Application.py
+++ sources/Application.py
@@ -798,16 +798,19 @@ class SetLevel(FSM):
         self.skybox.setDepthWrite(0)
         self.skybox.setLightOff()
         self.skybox.reparentTo(render)
-        dico = {"francais":0, "deutsch":1, "português":2}
+        dico = {"francais":0, "deutsch":1, "português":2, "english":3}
         self.textObject = OnscreenText(text="Veuillez choisir votre langue.", pos=(0, 0.7), scale=0.07, fg=(1, 0.5, 0.5, 1), align=TextNode.ACenter, mayChange=1)
-        self.menu = DirectOptionMenu(text="options", scale=0.15, pos=(-0.5, 0, 0), initialitem=dico[self.langue], items=["francais", "deutsch", "português"], highlightColor=(0.65, 0.65, 0.65, 1), command=self.itemSel, textMayChange=1)
+        self.menu = DirectOptionMenu(text="options", scale=0.15, pos=(-0.5, 0, 0), initialitem=dico[self.langue], items=["francais", "deutsch", "português", "english"], highlightColor=(0.65, 0.65, 0.65, 1), command=self.itemSel, textMayChange=1)
         self.exit_button = DirectButton(text="Retour", scale=0.07, pos=(-0.8, 1, -0.7), command=self.fade_out, extraArgs=["Trois_fichiers"])
         if self.langue == "deutsch":
             self.textObject.setText("Bitte wählen Sie Ihre Sprache.")
             self.exit_button.setText("Zurück")
         elif self.langue == "português":
-            self.textObject.setText("Favor escolhe o seu idioma ")
-            self.exit_button.setText("Voltar")    
+            self.textObject.setText("Favor escolhe o seu idioma.")
+            self.exit_button.setText("Voltar") 
+        elif self.langue == "english":
+            self.textObject.serText("Please, choose your language.")
+            self.exit_button.setText("Back")
 
     def itemSel(self, arg):
         """
@@ -825,8 +828,10 @@ class SetLevel(FSM):
             self.exit_button.setText("Zurück")
         elif self.langue == "português":
             self.textObject.setText("Favor escolhe o seu idioma ")
-            self.exit_button.setText("Voltar")     
-
+            self.exit_button.setText("Voltar") 
+        elif self.langue == "english":
+            self.textObject.serText("Please, choose your language.")
+            self.exit_button.setText("Back")
 
     def exitLanguage(self):
         """
